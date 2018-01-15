3c1b6380d21c4174e7e37639f35e178fb5d612e9
Nelson Araya <nelson@nelson-das>
2018-01-15 15:23:48 -0300

    cambios

--- resources/views/emergencia/vol.blade.php
+++ resources/views/emergencia/vol.blade.php
@@ -1,41 +1,4 @@
 @extends('layouts.main')
-@section('css')
-<style type="text/css">
-	
-.parpadea {
-  
-  animation-name: parpadeo;
-  animation-duration: 1s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-
-  -webkit-animation-name:parpadeo;
-  -webkit-animation-duration: 1s;
-  -webkit-animation-timing-function: linear;
-  -webkit-animation-iteration-count: infinite;
-}
-
-@-moz-keyframes parpadeo{  
-  0% { opacity: 1.0; }
-  50% { opacity: 0.0; }
-  100% { opacity: 1.0; }
-}
-
-@-webkit-keyframes parpadeo {  
-  0% { opacity: 1.0; }
-  50% { opacity: 0.0; }
-   100% { opacity: 1.0; }
-}
-
-@keyframes parpadeo {  
-  0% { opacity: 1.0; }
-   50% { opacity: 0.0; }
-  100% { opacity: 1.0; }
-}
-
-
-</style>
-@endsection
 @section('content')
 <div class="table-responsive">
 	<table class="table">
