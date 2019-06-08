6abe2b08d42e5979b8fd300c5f5af28183a89ee5
NelsonAraya <programmer.nelson@gmail.com>
2019-06-08 01:28:21 -0400

    1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,8 +50,8 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
 
-      //  parent::render($request, $exception);
-       return response()->view('error',[],500); //todo termina aca xD
+        parent::render($request, $exception);
+      // return response()->view('error',[],500); //todo termina aca xD
         
     }
 }
