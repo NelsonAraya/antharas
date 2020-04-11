fa88c89877319ad7a80398405ca49cb35147f4bc
Nelson Araya <programmer.nelson@gmail.com>
2020-04-11 18:37:38 -0400

    cambios para mensaje covid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,12 +48,15 @@
             <div class="panel-heading">ANTHARAS INFORMA</div>
                  <div class="panel-body">
                     El Staff De desarollo <b>ANTHARAS</b> junto con la <b>Comandancia</b> del <b>Cuerpo de bomberos de Iquique</b> les Informa.
+                         <img src="{{ asset('img/foto1.jpg') }}" class="img-responsive" alt="Responsive image">
+                          <!-- 
                            <ol>
                               <li>Descontaminación de calzado y lavado de manos con agua y jabón al menos por 20 segundos.</li>
                               <li>Descontamina las superficies de contacto y mantén distancia de al menos 1 metro siempre. </li>
                               <li>Prepara tu Epp para responder a emergencias y no olvides llevar la protección biológica. (Mascarilla, protección ocular y guantes de látex) </li>
                             </ol>
                             De todos depende mantenernos sanos y ganarle al <b>Covid-19.</b>  La operatividad institucional y de vuestra compañía es muy importante!!!
+                            -->
                  </div>
         </div>
     </div>
